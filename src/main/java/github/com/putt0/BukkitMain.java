58c8cb5cf110f20bbdddc63ede4f13c4e9560142
Putt0 <reinaldymc4@gmail.com>
2023-04-21 10:59:10 -0300

    Registering main commands.

--- src/main/java/github/com/putt0/BukkitMain.java
+++ src/main/java/github/com/putt0/BukkitMain.java
@@ -1,5 +1,7 @@
 package github.com.putt0;
 
+import github.com.putt0.commands.register.ReportCommand;
+import github.com.putt0.reasons.TabCompleteManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BukkitMain extends JavaPlugin {
@@ -16,6 +18,16 @@ public class BukkitMain extends JavaPlugin {
     protected void enableService() {
         try {
             System.out.print("[yReport] -> Iniciado!");
+        } catch (Exception exception) {
+            System.out.print("[yReport] -> Ocorreu um erro ao inicializar: (" + exception + ")");
+        }
+    }
+
+    protected void enableCommands() {
+        try {
+            getCommand("report").setExecutor(new ReportCommand());
+            getCommand("report").setTabCompleter(new TabCompleteManager());
+            System.out.print("[Commands] -> Iniciado!");
         } catch (Exception exception) {
             System.out.print("[yReport] -> Ocorreu um erro ao inicializar: (" + exception + ")");
         }
